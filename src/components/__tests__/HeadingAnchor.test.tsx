99eda06654aa53d1579ea6cf7e6b539830aa9931
Scott Turnbull <streamweaver@gmail.com>
2025-09-30 12:23:44 -0400

    tests:  Added post test cleanup.

--- src/components/__tests__/HeadingAnchor.test.tsx
+++ src/components/__tests__/HeadingAnchor.test.tsx
@@ -1,7 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
 import HeadingAnchor from "../HeadingAnchor";
 
 describe("HeadingAnchor", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   it("links to the provided heading id with accessible labelling", () => {
     render(<HeadingAnchor id="section-1" headingText="Section 1" />);
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,15 @@ export default defineConfig({
     projects: [
       {
         extends: true,
+        test: {
+          name: 'unit',
+          include: ['src/**/__tests__/**/*.test.{ts,tsx}'],
+          environment: 'jsdom',
+          globals: true,
+        },
+      },
+      {
+        extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
